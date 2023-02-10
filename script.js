bb3bf6f1f0fa4c299e0d3779389a740e7acb67c0
Warzzzzz <119421102+Warzzzzz@users.noreply.github.com>
2023-02-10 09:50:47 +0100

    JS

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ let cptB6= 0;
 let cptB7= 0;
 let timer = 0; 
 
+
+let bonusact1 = ['img/bonus_1.png'];
+let bonusact2 = ['img/bonus_2.png'];
+let bonusact3 = ['img/bonus_3.png'];
+let bonusact4 = ['img/bonus_4.png'];
+let bonusact5 = ['img/bonus_5.png'];
+let bonusact6 = ['img/bonus_6.png'];
+let bonusact7 = ['img/bonus_7.png'];
+
+
+let bonusDisplay = '';
+
 let audio1 = new Audio('piecemus1.mp3');
 let image1 = ['img/mario_2.png'];
 let img = document.querySelector("main");
@@ -25,41 +37,39 @@ function test(){
 
 
 function clics() {
+    
+    
     if(clicksTT===0){test()};
     clicks +=1 + bonusClick;
     clicksPI +=1 + bonusClick;
     clicksTT +=1;
     document.getElementById('clicks').innerHTML = "Nombre de clics : " + clicks ;
-    document.getElementById('clicksTT').innerHTML = "Nombre de clics totaux : " + clicksTT ;
+    document.getElementById('clicksTT').innerHTML = "Nombre de clics totaux : " + clicksTT 
 
     if (clicksPI >= 25) {
-            pieces +=1;
+            pieces +=9;
             clicksPI -=25;
             document.getElementById('pieces').innerHTML = "Nombre de piece obtenues: " + pieces;
     }
 
-    if (pieces >=9) {
+    if (pieces >=10) {
         audio1.play();
-        etoiles +=1;
-        pieces -=9
+        etoiles +=500;
+        pieces -=10
 
          
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
     }
 
-    if (clicksPI >= 25) {
-        pieces +=1;
-        clicksPI -=25;
-        document.getElementById('pieces').innerHTML = "Nombre de piece obtenues: " + pieces;
-    }
 }
 
 
 
 function bonus1() {
     if(cptB1!=0){
+       
         alert("Tu as déjà utilisé ce bonus !");
-    } 
+            } 
     if(etoiles<3) {
         alert("Tu n'as pas assez d'etoiles !");
     }
@@ -67,26 +77,33 @@ function bonus1() {
         bonusClick +=1;
         etoiles -=3;
         cptB1 +=1;
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact1+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_2.png";
+        
+
     } 
 
     
 }
 
 function bonus2() {
-    if(cptB2!=0){
+    if( cptB2!=0){
+       
         alert("Tu as déjà utilisé ce bonus !");
     } 
     if(etoiles<10) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=10  && cptB2==0) { 
+    if (etoiles >=10  && cptB1!=0 && cptB2==0) { 
         bonusClick +=1;
         etoiles -=10;
         cptB2 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_3.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact2+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
   
 
@@ -99,12 +116,14 @@ function bonus3() {
     if(etoiles<25) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=25  && cptB3==0) { 
+    if (etoiles >=25 && cptB1==1 && cptB2==1 && cptB3==0) { 
         bonusClick +=1;
         etoiles -=25;
         cptB3 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_4.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact3+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
   
 }
@@ -115,12 +134,14 @@ function bonus4() {
     if(etoiles <50) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=50  && cptB4==0) { 
+    if (etoiles >=50  && cptB1==1 && cptB2==1 && cptB3==1 && cptB4==0) { 
         bonusClick +=1;
         etoiles -=50;
         cptB4 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_5.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact4+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
 
   
@@ -132,12 +153,14 @@ function bonus5() {
     if(etoiles<100) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=100  && cptB5==0) { 
+    if (etoiles >=100  && cptB1==1 && cptB2==1 && cptB3==1 && cptB4==1 && cptB5==0) { 
         bonusClick +=1;
         etoiles -=100;
         cptB5 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_6.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact5+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
    
     
@@ -149,12 +172,14 @@ function bonus6() {
     if(etoiles<175) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=175  && cptB6==0) { 
+    if (etoiles >=175 && cptB1==1 && cptB2==1 && cptB3==1 && cptB4==1  && cptB5==1 && cptB6==0) { 
         bonusClick +=1;
         etoiles -=175;
         cptB6 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_7.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact6+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
  
    
@@ -166,13 +191,18 @@ function bonus7() {
     if(etoiles<250) {
         alert("Tu n'as pas assez d'etoiles !");
     }
-    if (etoiles >=250 && cptB7==0) { 
+    if (etoiles >=250 && cptB1==1 && cptB2==1 && cptB3==1 && cptB4==1  && cptB5==1 && cptB6==1 && cptB7==0) { 
         bonusClick +=1;
         etoiles -=250;
         cptB7 +=1;
         document.getElementById('etoiles').innerHTML = "Nombre d'etoiles obtenues: " + etoiles;
         document.getElementById('main-image').src = "img/mario_8.png";
+        bonusDisplay = bonusDisplay + '<img id="bonus" src="'+bonusact7+'" alt="" style="height: 20px"></img>';
+        document.getElementById('bonusImages').innerHTML = bonusDisplay;
     } 
  
+
+
+
 }
 
